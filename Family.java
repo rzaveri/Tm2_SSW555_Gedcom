e831e2c8a7fd3d32f2103e3ea99182a332eb39cb
Abhishek Shah <abhishek283@users.noreply.github.com>
2015-02-16 00:00:39 -0500

    Create Family.java

---
+++ Family.java
@@ -0,0 +1,29 @@
+//author:Abhishek Shah
+package SSW555.stevens.edu;
+
+public class Family 
+{
+	private String id;
+	private String husbandId;
+	private String wifeId;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getHusbandId() {
+		return husbandId;
+	}
+	public void setHusbandId(String husbandId) {
+		this.husbandId = husbandId;
+	}
+	public String getWifeId() {
+		return wifeId;
+	}
+	public void setWifeId(String wifeId) {
+		this.wifeId = wifeId;
+	}
+}
+
