45cb0ab878fee2dda47c73d87d0a37d887c01960
rzaveri <rzaveri@stevens.edu>
2015-02-15 23:59:22 -0500

    Created Individual Class
    Created Individual Class to store  properties of indidviduals

---
+++ Individual.java
@@ -0,0 +1,84 @@
+/*
+author Richa
+*/
+package SSW555.stevens.edu;
+
+import java.util.ArrayList;
+
+public class Individual 
+{
+	
+	private String id;
+	private String name;
+	private String sex;
+	private String birthDate;
+	private String deathDate;
+	private ArrayList<String> spouseInFamily;
+	private ArrayList<String> childInFamily;
+	
+//	public Individual() {
+//		this.setId("");
+//		this.setName("");
+//		this.setSex("");
+//		this.setBirthDate("");
+//		this.setDeathDate("");
+//		this.setSpouseInFamily(null);
+//		this.setChildInFamily(null);
+//	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public ArrayList<String> getChildInFamily() {
+		return childInFamily;
+	}
+
+	public void setChildInFamily(ArrayList<String> childInFamily) {
+		this.childInFamily = childInFamily;
+	}
+
+	public ArrayList<String> getSpouseInFamily() {
+		return spouseInFamily;
+	}
+
+	public void setSpouseInFamily(ArrayList<String> spouseInFamily) {
+		this.spouseInFamily = spouseInFamily;
+	}
+
+	public String getDeathDate() {
+		return deathDate;
+	}
+
+	public void setDeathDate(String deathDate) {
+		this.deathDate = deathDate;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
