384f6a84c2b726e5f206c617b86c81fae270c0e4
rzaveri <rzaveri@stevens.edu>
2015-03-16 01:54:25 -0400

    Marriagre before birth

--- GEDCOM/src/SSW555/stevens/edu/Family.java
+++ GEDCOM/src/SSW555/stevens/edu/Family.java
@@ -1,13 +1,15 @@
-//author: Richa Zaveri
 package SSW555.stevens.edu;
+
+import java.util.ArrayList;
 
 public class Family 
 {
 	private String id;
 	private String husbandId;
 	private String wifeId;
-        private String marriageDate;
-        private String divorceDate;
+	private String marriageDate;
+	private String divorceDate;
+	private ArrayList<String> children;
 	
 	public String getId() {
 		return id;
@@ -27,17 +29,24 @@ public class Family
 	public void setWifeId(String wifeId) {
 		this.wifeId = wifeId;
 	}
-        public String getMarriageDate() {
-            return marriageDate;
-        }
-         public void setMarriageDate(String marriageDate) {
-            this.marriageDate = marriageDate;
-         }
-        public String getDivorceDate() {
-            return divorceDate;
-        }
-         public void setDivorceDate(String divorceDate) {
-            this.divorceDate = divorceDate;
-        }
+	public String getMarriageDate() {
+		return marriageDate;
+	}
+	public void setMarriageDate(String marriageDate) {
+		this.marriageDate = marriageDate;
+	}
+	public String getDivorceDate() {
+		return divorceDate;
+	}
+	public void setDivorceDate(String divorceDate) {
+		this.divorceDate = divorceDate;
+	}
+	public ArrayList<String> getChildren() {
+		return children;
+	}
+
+	public void setChildren(ArrayList<String> children) {
+		this.children = children;
+	}
 }
 
